964ae4d13d00af81378cc320957a783d852caf75
sooncheol <sooncheolchoi7@gmail.com>
2019-08-07 03:32:04 +0900

    게터

--- ch11/todolistapp/src/components/List.vue
+++ ch11/todolistapp/src/components/List.vue
@@ -27,59 +27,76 @@ ul li.checked::before {
 </style>
 <template>
     <ul id="todolist">
-        <!-- <li v-for="a in todolist" :key="a.id" :class="checked(a.done)"
-            @click="doneToggle(a.id)"> 변경 -->
-            <li v-for="a in todolist" :key="a.id" :class="checked(a.done)"
-            @click="doneToggle({id: a.id})">
+        <li v-for="a in todolist" :key="a.id" :class="checked(a.done)"
+            @click="doneToggle(a.id)">
             <span>{{ a.todo }}</span>
             <span v-if="a.done"> (완료)</span>
-            <!-- <span class="close" v-on:click.stop="deleteTodo(a.id)">&#x00D7;</span> 변경 -->
-            <span class="close" @click.stop="deleteTodo({id: a.id})">&#x00D7;</span>
+            <span class="close" v-on:click.stop="deleteTodo(a.id)">&#x00D7;</span>
         </li>
     </ul>
 </template>
 <script type="text/javascript">
-import Constant from '../Constant' 
-import {mapState, mapMutations} from 'vuex' // 추가 (컴포넌트 바인딩 헬퍼 메서드 참조)
+// import eventBus from '../EventBus' 삭제
+import Constant from '../Constant' // 추가
 
 export default {
     name: 'List',
-    /* 변경
     computed : {
         todolist() {
             return this.$store.state.todolist; // 읽기전용 (가져옴)
         }
-    },*/
-    computed : mapState(['todolist']), 
-    // todolsit 상태를 계산형 속성에 자동 바인딩
-    // 여러 개의 상태를 바인딩하려면 배열에 상태 객체의 속성명을 나열하면 됩니다.
+    },
     methods : {
         checked : function(done){
             if(done) return {checked:true};
             else return {checked:false};
         },
-        /* 변경
         doneToggle: function(id){
             this.$store.commit(Constant.DONE_TOGGLE, {id:id})
+            // commit : Commit(‘[mutation메소드명]’)을 통해 Mutation
+            //          그룹에 속한 메서드를 실행할 수 있습니다.
+            // 화면에서 일어나는 이벤트를 받아서 변이를 일으키기 위해 $store.commit 메서드 호출
+            // $store.commit의 첫번째 인자는 변이의 이름, 두번째 인자는 변이에 전달할 인자
+            // 변이의 이름은 단순한 문자열이기 때문에 오타를 내기 쉬우므로 상수를 만들어서 사용하는
+            // 것을 권장합니다.
         },
         deleteTodo : function(id){
             this.$store.commit(Constant.DELETE_TODO, {id:id})
-        }*/
-        ...mapMutations([
-            Constant.DELETE_TODO,
-            Constant.DONE_TOGGLE
-        ])
-        // mapMutations 헬퍼 메서드는 변이를 동일한 이름의 메서드에 자동으로 연결합니다.
-        // 변이를 일으키지 않는 메서드가 함께 존재할 경우(checked)에는 mapMutations  
-        // 헬퍼 메서드가 만들어낸 객체와 변이를 일으키지 않는 메서드를 포함하는 객체를 ES6
-        // 문법인 전개연산자를 이용해서 병합합니다.
-
-        // 한 가지 주의할 점은 컴포넌트에서 mapMutations 헬퍼 메서드를 이용해 변이를
-        // 메서드에 바인딩한 경우 호출할 때 변이의 인자 형식을 따라야 합니다.
-        // @click="doneToggle(id: a.id)"
-        // @click.stop="deleteTodo(id: a.id)"
-
-        // 그림 11-09
+        }
     }
+    /* 삭제
+    created : function() {
+        eventBus.$on('add-todo', this.addTodo);
+    },
+    data : function() {
+        return {
+            todolist : [
+                { id:1, todo : "영화보기", done:false },
+                { id:2, todo : "주말 산책", done:true },
+                { id:3, todo : "ES6 학습", done:false },
+                { id:4, todo : "잠실 야구장", done:false },
+            ]
+        }
+    },
+    methods : {
+        checked : function(done) {
+            if(done) return { checked:true };
+            else return { checked:false };
+        },
+        addTodo : function(todo) {
+            if (todo !== "") {
+                this.todolist.push(
+                    { id:new Date().getTime(), todo : todo, done:false });
+            }
+        },
+        doneToggle : function(id) {
+            var index = this.todolist.findIndex((item)=>item.id === id);
+            this.todolist[index].done = !this.todolist[index].done;
+        },
+        deleteTodo : function(id) {
+            var index = this.todolist.findIndex((item)=>item.id === id);
+            this.todolist.splice(index,1);
+        }
+    }*/
 }
 </script>
